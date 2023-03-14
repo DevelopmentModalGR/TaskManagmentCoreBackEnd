e0dff4ffd672ffc6975956d615eafcac6ccd5b76
Renato Santos <renato.santos@modalgr.com.br>
2023-03-14 01:56:54 -0300

    fix: remove unecessary context

--- TaskManagmentCore.Api/Startup.cs
+++ TaskManagmentCore.Api/Startup.cs
@@ -93,7 +93,7 @@ namespace TaskManagmentCore.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext dataContext)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -135,7 +135,6 @@ namespace TaskManagmentCore.Api
             
 
             app.UseHttpsRedirection();
-            dataContext.Database.MigrateAsync();
         }
     }
 }
