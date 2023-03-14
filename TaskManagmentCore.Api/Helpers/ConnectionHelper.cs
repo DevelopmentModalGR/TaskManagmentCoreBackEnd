88c18dbd6c91b05192eecad2beafa67ce6e09c2b
Renato Santos <renato.santos@modalgr.com.br>
2023-03-14 01:59:27 -0300

    fix: comment all connection helper

--- TaskManagmentCore.Api/Helpers/ConnectionHelper.cs
+++ TaskManagmentCore.Api/Helpers/ConnectionHelper.cs
@@ -1,31 +1,31 @@
-using System;
-using Microsoft.Extensions.Configuration;
-using Npgsql;
+// using System;
+// using Microsoft.Extensions.Configuration;
+// using Npgsql;
 
-public static class ConnectionHelper
-    {
-        public static string GetConnectionString(IConfiguration configuration)
-        {
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
-            var databaseUrl = System.Environment.GetEnvironmentVariable("DATABASE_URL");
-            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
-        }
+// public static class ConnectionHelper
+//     {
+//         public static string GetConnectionString(IConfiguration configuration)
+//         {
+//             var connectionString = configuration.GetConnectionString("DefaultConnection");
+//             var databaseUrl = System.Environment.GetEnvironmentVariable("DATABASE_URL");
+//             return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+//         }
 
-        //build the connection string from the environment. i.e. Heroku
-        private static string BuildConnectionString(string databaseUrl)
-        {
-            var databaseUri = new Uri(databaseUrl);
-            var userInfo = databaseUri.UserInfo.Split(':');
-            var builder = new NpgsqlConnectionStringBuilder
-            {
-                Host = databaseUri.Host,
-                Port = databaseUri.Port,
-                Username = userInfo[0],
-                Password = userInfo[1],
-                Database = databaseUri.LocalPath.TrimStart('/'),
-                SslMode = SslMode.Require,
-                TrustServerCertificate = true
-            };
-            return builder.ToString();
-        }       
-    }
+//         //build the connection string from the environment. i.e. Heroku
+//         private static string BuildConnectionString(string databaseUrl)
+//         {
+//             var databaseUri = new Uri(databaseUrl);
+//             var userInfo = databaseUri.UserInfo.Split(':');
+//             var builder = new NpgsqlConnectionStringBuilder
+//             {
+//                 Host = databaseUri.Host,
+//                 Port = databaseUri.Port,
+//                 Username = userInfo[0],
+//                 Password = userInfo[1],
+//                 Database = databaseUri.LocalPath.TrimStart('/'),
+//                 SslMode = SslMode.Require,
+//                 TrustServerCertificate = true
+//             };
+//             return builder.ToString();
+//         }       
+//     }
